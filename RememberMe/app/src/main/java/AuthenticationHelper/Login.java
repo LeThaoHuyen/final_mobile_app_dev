26ac0ef69b0d60fd8cb1fc5c8e9e4f8036ac03cc
tmn04 <nmthu18@apcs.vn>
2021-08-22 13:41:43 +1000

    add login signup

---
+++ RememberMe/app/src/main/java/AuthenticationHelper/Login.java
@@ -0,0 +1,155 @@
+package AuthenticationHelper;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.rememberme.BarcodeScannerActivity;
+import com.example.rememberme.R;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+public class Login extends AppCompatActivity {
+
+    Button callSignUp, login_btn;
+    ImageView image;
+    TextView logoText, sloganText;
+    TextInputLayout username, password;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_login);
+
+        callSignUp = findViewById(R.id.signup_screen);
+        login_btn = findViewById(R.id.validate_login);
+
+        username = findViewById(R.id.log_username);
+        password = findViewById(R.id.log_password);
+
+        callSignUp.setOnClickListener((view) -> {
+                Intent intent =  new Intent(Login.this, SignUp.class);
+                startActivity(intent);
+        });
+
+        login_btn.setOnClickListener(new View.OnClickListener(){
+
+
+            @Override
+            public void onClick(View view) {
+                LoginUser(view);
+            }
+        });
+    }
+
+    private Boolean validateUsername()
+    {
+        String val = username.getEditText().getText().toString();
+        if(val.isEmpty())
+        {
+            username.setError("Field cannot be empty");
+            return false;
+        }
+        else
+        {
+            username.setError(null);
+            username.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    private Boolean validatePassword()
+    {
+        String val = password.getEditText().getText().toString();
+        if(val.isEmpty())
+        {
+            password.setError("Field cannot be empty");
+            return false;
+        }
+        else
+        {
+            password.setError(null);
+            password.setErrorEnabled(false);
+            return true;
+        }
+    }
+    public void LoginUser(View view)
+    {
+        if (validateUsername() | validatePassword())
+        {
+            isUser(view);
+        }
+    }
+
+    private void isUser(View view) {
+
+        final String userEnteredUsername= username.getEditText().getText().toString().trim();
+        final String userEnteredPassword= password.getEditText().getText().toString().trim();
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("users");
+        Query checkUser = reference.orderByChild("username").equalTo(userEnteredUsername);
+
+        checkUser.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    username.setError(null);
+                    username.setErrorEnabled(false);
+                    String passFromDB = dataSnapshot.child(userEnteredUsername).child("password").getValue(String.class);
+
+                    if (passFromDB.equals(userEnteredPassword)) {
+
+                        username.setError(null);
+                        username.setErrorEnabled(false);
+
+                        String nameFromDB = dataSnapshot.child(userEnteredUsername).child("name").getValue(String.class);
+                        String usernameFromDB = dataSnapshot.child(userEnteredUsername).child("username").getValue(String.class);
+                        String phoneNoFromDB = dataSnapshot.child(userEnteredUsername).child("phoneNo").getValue(String.class);
+                        String emailFromDB = dataSnapshot.child(userEnteredUsername).child("email").getValue(String.class);
+
+
+                        Intent intent =  new Intent(getApplicationContext(), BarcodeScannerActivity.class);
+
+
+                        intent.putExtra("name", nameFromDB);
+                        intent.putExtra("username", usernameFromDB);
+                        intent.putExtra("email", emailFromDB);
+                        intent.putExtra("phoneNo", phoneNoFromDB);
+                        intent.putExtra("password", passFromDB);
+
+                        startActivity(intent);
+                    }
+                    else {
+                        password.setError("Wrong Password");
+                        password.requestFocus();
+                    }
+                }
+                else
+                {
+                    username.setError("No such User exists");
+                    username.requestFocus();
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
+    }
+
+
+}
---
+++ RememberMe/app/src/main/java/AuthenticationHelper/SignUp.java
@@ -0,0 +1,138 @@
+package AuthenticationHelper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import com.example.rememberme.R;
+
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Calendar;
+
+import static java.util.Calendar.getInstance;
+
+public class SignUp extends AppCompatActivity {
+
+    TextInputLayout regName, regUsername, regEmail, regPhoneNo, regPassword;
+    Button regBtn, regToLoginBtn;
+
+    FirebaseDatabase rootNode;
+    DatabaseReference reference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+
+        regName = findViewById(R.id.reg_name);
+        regUsername = findViewById(R.id.reg_username);
+        regEmail = findViewById(R.id.reg_email);
+        regPhoneNo = findViewById(R.id.reg_phoneNo);
+        regPassword = findViewById(R.id.reg_password);
+        regToLoginBtn = findViewById(R.id.reg_login_btn);
+        regToLoginBtn.setOnClickListener((view)->{
+            Intent intent =  new Intent(SignUp.this, Login.class);
+            startActivity(intent);
+        });
+
+        regBtn = findViewById(R.id.reg_btn);
+        regBtn.setOnClickListener(new View.OnClickListener(){
+
+
+            @Override
+            public void onClick(View view) {
+
+                registerUser(view);
+            }
+
+        });
+    }
+
+    private Boolean validateName()
+    {
+        String val = regName.getEditText().getText().toString();
+
+        if(val.isEmpty())
+        {
+            regName.setError("Field cannot be empty");
+            return false;
+        }
+        else {
+            regName.setError(null);
+            return true;
+        }
+    }
+
+    private Boolean validateUsername()
+    {
+        String val = regUsername.getEditText().getText().toString();
+        String noWhiteSpace ="(?=\\S+$)";
+        if(val.isEmpty())
+        {
+            regUsername.setError("Field cannot be empty");
+            return false;
+        }
+        else if (val.length()>=15){
+            regUsername.setError("Username too long");
+            return false;
+        }
+        else if (val.matches(noWhiteSpace))
+        {
+            regUsername.setError("White Spaces are not allowed");
+            return false;
+        }
+        else
+        {
+            regUsername.setError(null);
+            return true;
+        }
+    }
+
+    private Boolean validateEmail()
+    {
+        String val = regEmail.getEditText().getText().toString();
+        String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+        if(val.isEmpty()) {
+            regEmail.setError("Field cannot be empty");
+            return false;
+        }
+        else if (!val.matches(emailPattern))
+        {
+            regEmail.setError("Invalid email");
+            return false;
+        }
+        else
+        {
+            regEmail.setError(null);
+            return true;
+        }
+
+    }
+    public void registerUser(View view)
+    {
+        if (!validateEmail() | !validateName() | !validateUsername()) {
+            return;
+        }
+
+        String name = regName.getEditText().getText().toString();
+        String username = regUsername.getEditText().getText().toString();
+        String email = regEmail.getEditText().getText().toString();
+        String phoneNo = regPhoneNo.getEditText().getText().toString();
+        String password = regPassword.getEditText().getText().toString();
+
+        rootNode = FirebaseDatabase.getInstance();
+        reference = rootNode.getReference("users");
+
+        UserHelperClass helperClass = new UserHelperClass(name, username, email, phoneNo, password);
+        reference.child(username).setValue(helperClass);
+
+        Intent intent =  new Intent(SignUp.this, Login.class);
+        startActivity(intent);
+    }
+}
